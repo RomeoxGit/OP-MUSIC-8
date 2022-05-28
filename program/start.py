964181e4140da2ba99fa398300a963bbeb238c4c
RomeoxGit <98481727+RomeoxGit@users.noreply.github.com>
2022-05-28 17:06:18 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -98,17 +98,13 @@ Check out all of my commands in the help Button given below.
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("➕ Add me to a Group ➕", url=f"https://t.me/{me_bot.username}?startgroup=true")
+                    InlineKeyboardButton("✚ Click here to Add Me", url=f"https://t.me/{me_bot.username}?startgroup=true")
                 ],[
-                    InlineKeyboardButton("❓ Basic Guide", callback_data="user_guide")
+                    InlineKeyboardButton("📨 Support", url=f"https://t.me/{GROUP_SUPPORT}"),
+                    InlineKeyboardButton("📨 Updates", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],[
-                    InlineKeyboardButton("📚 Commands", callback_data="command_list"),
-                    InlineKeyboardButton("❤️ Donate", url=f"https://t.me/{OWNER_USERNAME}")
-                ],[
-                    InlineKeyboardButton("👥 Support Group", url=f"https://t.me/{GROUP_SUPPORT}"),
-                    InlineKeyboardButton("📣 Support Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
-                ],[
-                    InlineKeyboardButton("🌐 Source Code", url="https://github.com/levina-lab/video-stream")
+                    InlineKeyboardButton("🗂 Commands", url=f"https://t.me/{GROUP_SUPPORT}"),
+                    InlineKeyboardButton("📍Info", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],
             ]
         ),
